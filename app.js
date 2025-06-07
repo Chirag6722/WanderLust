25622c82acf060efc648b004c35a468a27a35bd6
Chirag6722 <chiraghonnyal6722@gmail.com>
2025-06-07 09:46:45 +0530

    Progress

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -25,34 +27,48 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(methodOverride("_method"));
-app.use(express.urlencoded({ extended: true }));
 
-// Root route
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
-// Catch-all 404 Route
-app.all(/.*/, (req, res, next) => {
-  next(new ExpressError(404, "Page Not Found"));
+app.use((req, res, next) => {  // For all undefined routes
+  next(new ExpressError(404, "Page Not Found!"));
 });
 
-
-
-// Error Handler Middleware
-app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs", { message });
+app.use((err, req, res, next) => { // error handler middleware
+  let {statusCode = 500, message = "Something went wrong!"} = err;
+  res.status(statusCode).render("error.ejs", {message});
+  // res.status(statusCode).send(message);
 });
 
-// Start the server
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
-
--- models/listing.js
+++ models/listing.js
@@ -1,40 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+// const review = require('./review');
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
 
 const listingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-},
- description: String,
- image: {
-   type: String,
-   default:"https://unsplash.com/photos/coconut-tree-near-shore-within-mountain-range-RN6ts8IZ4_0",
-   set: (v) =>
-     v === ""
-      ? "https://unsplash.com/photos/coconut-tree-near-shore-within-mountain-range-RN6ts8IZ4_0"
-      : v,
-},
-price: Number,
-location: String,
-country: String,
-reviews: [
-  {
-    type: Schema.Types.ObjectId,
-    ref: "Review",
-  },
-],
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String
+    },
+    image : {
+        type : String,
+        default : "https://images.unsplash.com/photo-1747607176057-175b357ef4ab?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1747607176057-175b357ef4ab?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+    },
+    price : {
+        type : Number
+    },
+    location : {
+        type : String
+    },
+    country : {
+        type : String 
+    },
+    reviews : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ] 
 });
 
-listingSchema.post("findOneAndDelete", async (listing) => {
-  if (listing) {
-    await Review.deleteMany({ _id: { $in: listing.reviews } });
-  }
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if (listing) {
+        await Review.deleteMany({_id : {$in : listing.reviews}});
+    }
 });
-
-
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
-
