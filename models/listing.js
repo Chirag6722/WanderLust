6665be1834b77dede8205ca0f3e379511d20fe86
Chirag6722 <chiraghonnyal6722@gmail.com>
2025-06-08 14:18:01 +0530

    added option for adding image

--- models/listing.js
+++ models/listing.js
@@ -12,9 +12,8 @@ const listingSchema = new Schema({
         type : String
     },
     image : {
-        type : String,
-        default : "https://images.unsplash.com/photo-1747607176057-175b357ef4ab?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1747607176057-175b357ef4ab?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+       url: String,
+       filename: String,
     },
     
     price: Number,
--- routes/listing.js
+++ routes/listing.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn , isOwner, validatelisting} = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage })
 
 router
  .route("/")
  .get(wrapAsync(listingController.index))
- .post(isLoggedIn, validatelisting, wrapAsync(listingController.createListing)
+ .post(
+  isLoggedIn,
+  
+  upload.single("listing[image]"),
+  validatelisting,
+   wrapAsync(listingController.createListing)
 );
+
 
 //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
