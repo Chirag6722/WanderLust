ddb2df50bd1336cdb7b2d1a6cc87c513cb618ac8
Chirag6722 <chiraghonnyal6722@gmail.com>
2025-06-25 10:13:01 +0530

    image fitted

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,26 +1,46 @@
 const Listing = require("../models/listing");
 
+// Helper: highlight matched search terms (escape regex safely)
+function highlightMatch(text, searchTerm) {
+  if (!text || !searchTerm) return text;
+  const escaped = searchTerm.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  const regex = new RegExp(`(${escaped})`, 'ig');
+  return text.replace(regex, '<mark>$1</mark>');
+}
+
 module.exports.index = async (req, res) => {
   const { category, location } = req.query;
-  let listings = [];
+  let filter = {};
 
-  const filter = {};
-  if (category) filter.category = category;
-  if (location) filter.location = new RegExp(location, 'i');
+  if (category) {
+    filter.category = category;
+  }
 
-  listings = await Listing.find(filter);
-  
-  res.render("listings/index.ejs", { 
-    allListings: listings, 
-    selectedCategory: category || "", 
-    searchLocation: location || "" 
+  if (location) {
+    filter.$or = [
+      { title: new RegExp(location, 'i') },
+      { location: new RegExp(location, 'i') },
+      { country: new RegExp(location, 'i') }
+    ];
+  }
+
+  let listings = await Listing.find(filter).lean();
+
+  if (location) {
+    listings = listings.map(l => ({
+      ...l,
+      title: highlightMatch(l.title, location),
+      location: highlightMatch(l.location, location),
+      country: highlightMatch(l.country, location)
+    }));
+  }
+
+  res.render("listings/index.ejs", {
+    allListings: listings,
+    selectedCategory: category || "",
+    searchLocation: location || ""
   });
 };
-
-
-
-
-
 
 module.exports.renderNewForm = (req, res) => {
   res.render("listings/new.ejs");
@@ -28,22 +48,27 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id).populate({path: "reviews", populate: {path: "author"}}).populate("owner");
-  if(!listing) {
+  const listing = await Listing.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+
+  if (!listing) {
     req.flash("error", "Listing you requested for does not exist!");
-    res.redirect("/listings");
+    return res.redirect("/listings");
   }
+
   res.render("listings/show.ejs", { listing });
 };
 
-module.exports.createListing = async (req, res, next) => {
+module.exports.createListing = async (req, res) => {
   let url = req.file.path;
   let filename = req.file.filename;
 
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image = {url, filename};
+  newListing.image = { url, filename };
   await newListing.save();
+
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
@@ -51,25 +76,27 @@ module.exports.createListing = async (req, res, next) => {
 module.exports.renderEditForm = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
-  if(!listing) {
+
+  if (!listing) {
     req.flash("error", "Listing you requested for does not exist!");
-    res.redirect("/listings");
+    return res.redirect("/listings");
   }
 
-  let originalImageUrl = listing.image.url;
-  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250,h_300,c_fit");
+  let originalImageUrl = listing.image.url.replace("/upload", "/upload/w_250,h_300,c_fit");
   res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  if(typeof req.file !== "undefined") {
+
+  if (req.file) {
     let url = req.file.path;
     let filename = req.file.filename;
-    listing.image = {url, filename};
+    listing.image = { url, filename };
     await listing.save();
   }
+
   req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
 };
